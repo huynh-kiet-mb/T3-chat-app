bc0f0b51715d37f680176f7949de5193d2d0a6b3
huynh-kiet-mb <kiet.huynh@muaban.net>
2022-10-31 16:18:26 +0700

    git branch -M main

---
+++ src/pages/_app.tsx
@@ -0,0 +1,84 @@
+// src/pages/_app.tsx
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+import type { AppType } from "next/app";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import { wsLink, createWSClient } from "@trpc/client/links/wsLink";
+import { AppRouter } from "../server/trpc/router/_app";
+import superjson from "superjson";
+import { withTRPC } from "@trpc/next";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+  if (process.env.VERCEL_URL) {
+    // SSR should use vercel url
+    return `http://${process.env.VERCEL_URL}`;
+  }
+  // dev SSR should use localhost
+  return `http://${process.env.PORT ?? 3000}`;
+};
+
+const url = `${getBaseUrl()}/api/trpc`;
+
+function getEndingLink() {
+  if (typeof window === "undefined") {
+    return httpBatchLink({
+      url,
+    });
+  }
+
+  const client = createWSClient({
+    url: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:3001",
+  });
+
+  return wsLink<AppRouter>({
+    client,
+  });
+}
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+
+    return {
+      links: [
+        // pass logger link in here
+        getEndingLink(),
+      ],
+      transformer: superjson,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      headers: () => {
+        if (ctx?.req) {
+          return {
+            ...ctx.req.headers,
+          };
+        }
+        return {};
+      },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: false,
+})(MyApp);
